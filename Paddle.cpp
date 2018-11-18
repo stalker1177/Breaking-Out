a720ae2739b2d205b81ae8a2a231c66ee3229771
stalker1177 <44020451+stalker1177@users.noreply.github.com>
2018-11-18 23:36:11 +0300

    Commited to the git

---
+++ Paddle.cpp
@@ -0,0 +1,5 @@
+//
+// Created by stalk on 24.11.2018.
+//
+
+#include "Paddle.h"
---
+++ Paddle.h
@@ -0,0 +1,14 @@
+//
+// Created by stalk on 24.11.2018.
+//
+
+#ifndef BREAKINGOUT_PADDLE_H
+#define BREAKINGOUT_PADDLE_H
+
+
+class Paddle {
+
+};
+
+
+#endif //BREAKINGOUT_PADDLE_H
